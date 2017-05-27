79f7d7e9a382a4ac268a1666863458700428594b
kevin@suneee <56701117@qq.com>
2017-05-27 18:10:04 +0800

    guihua jiekou

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,18 @@ Route::group(['prefix'=>'/v1','middleware'=>['api']],function (){
 });
 
 //Route::post('/goods/one','Home\GoodsController@getOne');
-Route::post('/goods/home_goods','Home\GoodsController@getShopHomeGoods');
-Route::post('/goods/detail','Home\GoodsController@getGoodsDetail');
+Route::post('/goods/goodslist','Home\GoodsController@getShopHomeGoods');
+Route::post('/goods/goodsinfo','Home\GoodsController@getGoodsDetail');
+Route::post('/user/notice','Home\GoodsController@getGoodsDetail');//关注
+Route::post('/user/collection','Home\GoodsController@getGoodsDetail');//收藏
+Route::post('/user/userinfo','Home\GoodsController@getGoodsDetail');
+Route::post('/order/all','Home\GoodsController@getGoodsDetail');//全部订单
+Route::post('/order/bf_pay','Home\GoodsController@getGoodsDetail');//待付款
+Route::post('/order/bf_ship','Home\GoodsController@getGoodsDetail');//待发货
+Route::post('/order/bf_de','Home\GoodsController@getGoodsDetail');//待收货
+Route::post('/order/xxxprefect','Home\GoodsController@getGoodsDetail');//已完成
+//购物车（添加、列表、删除商品）
+//我的购物记录
+//购物流程
+
+
