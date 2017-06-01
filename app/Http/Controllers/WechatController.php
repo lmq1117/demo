0c85ce2476d73879b6c10de10375c41b7bd7a95d
kevin@suneee <56701117@qq.com>
2017-06-01 15:21:12 +0800

    回个图

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -15,6 +15,44 @@ class WechatController extends Controller
         Log::info('----Wechat request----'.json_encode($req_data));
         Log::info('----Wechat request2----'.$postStr);
 
+        //将xml字符串转换为对象
+        $postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
+
+        $fromUsername = $postObj->FromUserName;    //谁发给我的
+        $toUsername = $postObj->ToUserName;         //发给谁的 公众号的微信号
+        $keyword = trim($postObj->Content);         //发的啥 文本消息有
+
+        if($postObj->MsgType == 'text'){
+            //文本消息
+        } elseif ($postObj->MsgType == 'event'){
+            //事件
+            switch ($postObj->Event){
+                case 'CLICK':
+
+                    break;
+                case 'VIEW':
+                    if($postObj->MenuId == '412801741'){
+                        //进入首页了
+                        //根据openid去微信服务器拿资料
+                        //$userInfo = getUserInfo($postObj->FromUserName);
+                        //file_put_contents('/tmp/wco_user_info.log',date('Y-m-d H:i:s',time()).'----'.json_encode($userInfo)."\r\n",FILE_APPEND);
+                        //写session
+
+                    }
+                    break;
+            }
+
+        }
+
+        return "<xml>
+        <ToUserName><![CDATA[".$fromUsername."]]></ToUserName>
+        <FromUserName><![CDATA[".$toUsername."]]></FromUserName>
+        <CreateTime>".time()."</CreateTime>
+        <MsgType><![CDATA[image]]></MsgType>
+        <Image>
+        <MediaId><![CDATA[ABHRIwdrVPsHuqtJ0zt8RPoPzoySI1cy8Xh29oe869nEU9MSpvdlCSiwFyObbEGn]]></MediaId>
+        </Image>
+        </xml>";
 
 
 
