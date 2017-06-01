276515b3738dea1ded5b66d5273970373648ae97
LimgQuan <lmq1117@qq.com>
2017-06-01 23:58:09 +0800

    toArray TEST 4

--- app/Http/Controllers/Home/GoodsController.php
+++ app/Http/Controllers/Home/GoodsController.php
@@ -18,8 +18,8 @@ class GoodsController extends ApiTmpController
     //
     public function test(){
         //return 'goods_test----'.date('Y-m-d H:i:s',time());
-        $user = DB::table('users')->where('wx_openid','outktv28lv2UjvPTeT1TvKRRx0tc')->first();
-        $user->toArray();
+        //$user = DB::table('users')->where('wx_openid','outktv28lv2UjvPTeT1TvKRRx0tc')->first();
+        //$user->toArray();
         //var_dump($openid);
         //$arr = Cache::get('userInfo_outktv28lv2UjvPTeT1TvKRRx0tc');
         //$arr = [
@@ -39,7 +39,11 @@ class GoodsController extends ApiTmpController
         //$res = cache(['accessToken'=>1],1);
         //var_dump(date('Y-m-d H:i:s',time()));
         //var_dump($res);
-        var_dump($user);
+
+        //var_dump($user);
+
+        $user = User::find(1);
+        return $user->toArray();
 
 
     }
