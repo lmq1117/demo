f40db39975f83d44bb8e5e9b7ecb241219aefd86
LimgQuan <lmq1117@qq.com>
2017-06-04 12:02:17 +0800

    购物车修改

--- app/Http/Controllers/Home/ShoppingCartController.php
+++ app/Http/Controllers/Home/ShoppingCartController.php
@@ -28,13 +28,13 @@ class ShoppingCartController extends CommonController
         $g_id = $req_data['g_id'];
         $cart_num = $req_data['num'];//加入购物车的商品数量
 
-        $act = $req_data['act'];
+        $opearte = $req_data['operate'];
 
-        switch ($act){
+        $goods = Goods::find($g_id);
+        $session_str = $this->session->get($session_id);
+        $session = json_decode($session_str,true);
+        switch ($opearte){
             case 'add':
-
-                $goods = Goods::find($g_id);
-                $session = $this->session->get($session_id);
 
                 //购物车里边有该商品，增加商品数量
                 if($session['shopping_cart'][$g_id]){
@@ -48,11 +48,22 @@ class ShoppingCartController extends CommonController
                 }
                 //将商品信息查出来，拼接上商品数量，加入购物车
                 //取出session 里边的购物车信息，
+                //$this->session->set($session_id,json_encode($session));
+                break;
 
-                $this->session->set($session_id,$session);
+            case 'edit':
+                $goods = $session['shopping_cart'][$g_id];
+                $goods->cart_num = $cart_num;
+                $session['shopping_cart'][$g_id] = $goods;
+                //$this->session->set($session_id,json_encode($session));
+                break;
+            case 'del':
+                unset($session['shopping_cart'][$g_id]);
+                //$this->session->set($session_id,json_encode($session));
                 break;
 
         }
+        $this->session->set($session_id,json_encode($session));
 
 
 
