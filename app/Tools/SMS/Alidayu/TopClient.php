87f1492d38ce8fa6abc0041471604d964565f4c0
kevin@suneee <56701117@qq.com>
2017-06-05 09:11:29 +0800

    阿里大于test

--- app/Tools/SMS/Alidayu/TopClient.php
+++ app/Tools/SMS/Alidayu/TopClient.php
@@ -329,7 +329,8 @@ class TopClient
 		if (isset($respObject->code))
 		{
 			$logger = new TopLogger;
-			$logger->conf["log_file"] = rtrim(TOP_SDK_WORK_DIR, '\\/') . '/' . "logs/top_biz_err_" . $this->appkey . "_" . date("Y-m-d") . ".log";
+			//$logger->conf["log_file"] = rtrim(TOP_SDK_WORK_DIR, '\\/') . '/' . "logs/top_biz_err_" . $this->appkey . "_" . date("Y-m-d") . ".log";
+			$logger->conf["log_file"] =  '/opt/www/demo/storage/' . "logs/top_biz_err_" . $this->appkey . "_" . date("Y-m-d") . ".log";
 			$logger->log(array(
 				date("Y-m-d H:i:s"),
 				$resp
