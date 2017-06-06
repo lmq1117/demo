8e44a14dc58c2919321814b753106f53cb1a5563
kevin@suneee <56701117@qq.com>
2017-06-06 14:42:12 +0800

    return

--- app/Entity/Home/Order.php
+++ app/Entity/Home/Order.php
@@ -14,6 +14,6 @@ class Order extends Model
      * 订单状态$type 0待付款，1已付款待发货，2已付款已发货，3已付款已收货，4客户申请退款，5客户申请退款并被审核，6退款完成
      */
     public static function getOrderStatusCount($user_id,$type){
-        self::where('status',$type)->where('u_id',$user_id)->count();
+        return self::where('status',$type)->where('u_id',$user_id)->count();
     }
 }
