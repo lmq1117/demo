45def4c32852c09da68b3b18c937a44e3fb0ff0c
LimgQuan <lmq1117@qq.com>
2017-06-07 00:14:42 +0800

    注释掉创建菜单方法

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,9 @@ Route::get('/test','Home\GoodsController@test');
 Route::get('/stest','Home\GoodsController@sessionTest');
 Route::get('/cadd','Home\GoodsCollectionController@goodsCollectionAdd');
 Route::get('/nlist','Home\GoodsController@goodsNoticeList');
-Route::get('/makemenu','WechatController@createMemu');
+
+//生成菜单方法，不用时请注释掉
+//Route::get('/makemenu','WechatController@createMemu');
 
 //验证微信URL时才开，不验证不开
 //Route::any('/wechat','WechatController@verifyUrl');
